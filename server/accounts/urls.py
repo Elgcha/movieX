c6e6a54465d9ea3e52c0bf9c21a99eed3cc20ed6
Elgcha <vkdlfl681@gmail.com>
2021-11-16 14:57:23 +0900

    basic

---
+++ server/accounts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # path('signup/', views.signup),
+    # path('login/', views.login),
+    # path('update/', views.update),
+    # path('delete/', views.delete),
+    # path('password/', views.password_change),
+]
---
+++ server/movies/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class People(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    gender = models.IntegerField()
+    known_for_department = models.CharField(max_length=100)
+    also_known_as = models.JSONField()
+
+
+class Movie(models.model):
+    title = models.CharField(max_length=100)
+    overview = models.TextField()
+    release_date = models.DateField()
+    # popularity
+    poster_path = models.TextField()
+    vote_count = models.IntegerField()
+    vote_average = models.FloatField()
+    adult = models.BooleanField()
+    original_title = models.CharField()
+    genres = models.ManyToManyField(Genre, related_name='genre_movies')
+    people = models.ManyToManyField(People, related_name='people_movies')
+
+    def __str__(self):
+        return self.title
