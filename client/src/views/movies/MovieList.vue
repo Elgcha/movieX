ec088ee151134ce11c01b1ae8f30bdc21efb9176
Elgcha <vkdlfl681@gmail.com>
2021-11-17 15:11:23 +0900

    MOV-29 movielist

--- client/src/views/movies/MovieList.vue
+++ client/src/views/movies/MovieList.vue
@@ -1,12 +1,34 @@
 <template>
-  <div></div>
+  <div>
+    <div v-if="movies[0]">
+      <div v-for="movie in movies" :key="movie.id" class='col-2 p-2 h-100 d-inline-block' @click="moveToDetail(movie.id)">
+        <div v-if='movie' class='card popular'>
+          <img :src="'https://image.tmdb.org/t/p/w500/' + movie.poster_path" alt="popularmovie" class="img-responsive img-rounded">
+          <div class="overlay">
+            <h3 class="description">{{ movie.title }}</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import axios from 'axios'
+import {mapActions} from 'vuex'
+
+
 export default {
   name: 'MovieList',
+  data: function () {
+    return {
+      movies: [],
+    }
+  },
   methods: {
+    ...mapActions([
+      'moveToDetail',
+    ]),
     getMovies: function() {
       axios({
         methods: 'get',
@@ -14,6 +36,7 @@ export default {
       })
         .then(res => {
           console.log(res)
+          this.movies = res.data
         })
     }
   },
